396ed451188dc862feb4d351a8b963226b9c13b7
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-08-01 20:49:35 +1200

    Created index and style.css

---
+++ student/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Library Management System</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+
+<body>
+    <div class="wrapper">  
+        <header>
+            <div class="logo">
+                <img src="images/9.png" alt="">
+                <h1 style="color: white">ONLINE LIBRARY MANAGEMENT SYSTEM</h1>
+            </div>
+
+            <nav>
+                <ul>
+                    <li><a href="">HOME</a></li>
+                    <li><a href="">BOOKS</a></li>
+                    <li><a href="">STUDNET-LOGIN</a></li>
+                    <li><a href="">ADMIN_LOGIN</a></li>
+                    <li><a href="">FEEDBACK</a></li>
+
+                </ul>
+            </nav>
+        </header>
+    <section>
+        <br><br><br>
+            <div class="box">
+                <br><br><br><br>
+                <h1 style="text-align: center; font-size: 30px;">Welcome to library</h1><br>
+                <h1 style="text-align: center;font-size: 25px;">Opens at 09:00</h1><br>
+                <h1 style="text-align: center;font-size: 25px;">Closes at 15:00</h1><br>
+            </div>
+    </section>
+    <footer>
+
+    </footer>
+</div>
+</body>
+</html> 
