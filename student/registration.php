58cd7b45d46e785d8308ef5990e0c5a36ba19eff
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-08-02 09:16:43 +1200

    Abilitiy to POST registration details

--- student/registration.php
+++ student/registration.php
@@ -1,9 +1,8 @@
+
 <?php
-  include 'connection.php';
-  include 'navbar.php';
-
+  include "connection.php";
+  include "navbar.php";
 ?>
-
 
 <!DOCTYPE html>
 <html>
@@ -13,47 +12,29 @@
   <link rel="stylesheet" type="text/css" href="style.css">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
 
   <style type="text/css">
-    section {
+    section
+    {
       margin-top: -20px;
     }
-  </style>
-
+  </style>   
 </head>
 <body>
-<header style="height: 90px;">
-<!-- <nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-      <div class="navbar-header">
-          <a class="navbar-brand active">ONLINE LIBRARY MANAGEMENT SYSTEM</a>
-      </div>
-
-        <ul class="nav navbar-nav">
-            <li><a href="index.html">HOME</a></li>
-            <li><a href="">BOOKS</a></li>
-            <li><a href="">FEEDBACK</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li ><a href="student_login.html"><span class="glyphicon glyphicon-log-in"> LOGIN</span></a></li>
-            <li ><a href="student_login.html"><span class="glyphicon glyphicon-log-out"> LOGOUT</span></a></li>
-            <li><a href="registration.html"><span class="glyphicon glyphicon-user"> SIGN UP</span></a></li>
-          </ul>
-
-      </div>
-    </nav> -->
 
 <section>
   <div class="reg_img">
-        <br>
+
     <div class="box2">
-        <h1 style="text-align: center; font-size: 35px;font-family: Lucida Console;"> &nbsp Library Management System</h1>
-        <h1 style="text-align: center; font-size: 25px;">User Registration Form</h1><br>
-      <form name="Registration" action="" method="">
-        <br>
+        <h1 style="text-align: center; font-size: 35px;font-family: Lucida Console;"> &nbsp &nbsp &nbsp  Library Management System</h1>
+        <h1 style="text-align: center; font-size: 25px;">User Registration Form</h1>
+
+      <form name="Registration" action="" method="post">
+        
         <div class="login">
           <input class="form-control" type="text" name="first" placeholder="First Name" required=""> <br>
           <input class="form-control" type="text" name="last" placeholder="Last Name" required=""> <br>
@@ -62,11 +43,55 @@
           <input class="form-control" type="text" name="roll" placeholder="Roll No" required=""><br>
 
           <input class="form-control" type="text" name="email" placeholder="Email" required=""><br>
-          <input class="btn btn-default" type="text" type="submit" name="submit" value="Sign Up" style="color:black; width: 80px; height: 30px;" >
-        </div>
-      </form>
+          <input class="form-control" type="text" name="contact" placeholder="Phone No" required=""><br>
 
-    
+          <input class="btn btn-default" type="submit" name="submit" value="Sign Up" style="color: black; width: 70px; height: 30px"> </div>
+      </form>
+     
+    </div>
   </div>
 </section>
+
+    <?php
+
+      if(isset($_POST['submit']))
+      {
+        $count=0;
+        $sql="SELECT username from `student`";
+        $res=mysqli_query($db,$sql);
+
+        while($row=mysqli_fetch_assoc($res))
+        {
+          if($row['username']==$_POST['username'])
+          {
+            $count=$count+1;
+          }
+        }
+        if($count==0)
+        {
+          mysqli_query($db,"INSERT INTO `STUDENT` VALUES('$_POST[first]', '$_POST[last]', '$_POST[username]', '$_POST[password]', '$_POST[roll]', '$_POST[email]', '$_POST[contact]');");
+        ?>
+          <script type="text/javascript">
+           alert("Registration successful");
+          </script>
+        <?php
+        }
+        else
+        {
+
+          ?>
+            <script type="text/javascript">
+              alert("The username already exist.");
+            </script>
+          <?php
+
+        }
+
+      }
+
+    ?>
+
 </body>
+</html>
+registration.php
+Displaying registration.php.
