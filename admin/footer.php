001b7fbf4449a19726fb49c3abafbbf713bb2b22
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-08-03 10:04:08 +1200

    Copied student files in admin folder

---
+++ admin/footer.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Library Management System</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style type="text/css">
+         .fa 
+       
+       {   
+           
+           padding: 5px;
+           font-size: 20px;
+           width: 20px;
+           height: 20px;
+          border-radius: 50%;
+           text-decoration: none;
+           
+       }
+       .fa:hover{
+           opacity: .7;
+       }
+       .fa-facebook 
+       {
+           background: #3B5998;
+           color: white;
+       }
+       .fa-twitter 
+       {
+           background: #55ACEE;
+           color: white;
+       }
+       .fa-google 
+       {
+           background: #dd4b39;
+           color: white;
+       }
+       .fa-instagram 
+       {
+           background: #125688;
+           color: white;
+       }
+       .fa-yahoo 
+       {
+           background: #400297;
+           color: white;
+       }
+    </style>
+</head>
+<body>
+    <footer style="background-color: black;">
+         
+<br>
+<h3 style="color:white;text-align: center;"> Contact us through social media </h3><br>
+
+	<div style="margin:-4px 570px; text-align: center; ">
+
+		<a href="#" class="fa fa-facebook"></a>
+		<a href="#" class="fa fa-twitter"></a>
+		<a href="#" class="fa fa-google"></a>
+		<a href="#" class="fa fa-instagram"></a>
+		<a href="#" class="fa fa-yahoo"></a>
+	</div>
+
+	<br><br><br>
+	<p style="color:white;text-align: center;">
+		<br>
+		Email:&nbsp Online.library@gmail.com <br><br>
+		Mobile:&nbsp &nbsp +880171*******
+	</p>
+    </footer>
+    
+</body>
+</html>
---
+++ admin/student_login.php
@@ -0,0 +1,109 @@
+<?php
+ include 'connection.php';
+ include 'navbar.php';
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Login</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+
+    <style type="text/css">
+    section
+    {
+      margin-top: -20px;
+    }
+  </style>  
+  
+</head>
+<body>
+<!-- <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+          <a class="navbar-brand active">ONLINE LIBRARY MANAGEMENT SYSTEM</a>
+      </div>
+
+        <ul class="nav navbar-nav">
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="books.php">BOOKS</a></li>
+            <li><a href="feedback.php">FEEDBACK</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li ><a href="student_login.php"><span class="glyphicon glyphicon-log-in"> LOGIN</span></a></li>
+            <li ><a href="student_login.php"><span class="glyphicon glyphicon-log-out"> LOGOUT</span></a></li>
+            <li><a href="registration.php"><span class="glyphicon glyphicon-user"> SIGN UP</span></a></li>
+          </ul>
+
+      </div>
+    </nav> -->
+   
+    <section>
+    <div class="log_img">
+    <br> <br><br>
+          <div class="box1">
+              <h1 style="text-align: center; font-size: 35px;font-family: Lucida Console;">Library Management System</h1>
+              <h1 style="text-align: center; font-size: 25px;">User Login Form</h1><br>
+            <form name="login" action="" method="post">
+           
+              <div class="login">
+                <input class="form-control" type="text" name="username" placeholder="Username" required=""> <br><br>
+                <input class="form-control" type="password" name="password" placeholder="Password" required=""> <br>
+                <input class="btn btn-default"  type="submit" name="submit" value="Login" style="color:black; width: 60px; height: 30px;" ></div>
+            <p style="color: white; padding-left: 15px;">
+              <br><br>
+              <a style="color:white;" href="">Forgot password?</a> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+              New to this website?<a style="color: white;" href="registration.php"> Sign Up</a>
+            </p>
+            </form>
+          </div>
+        </div>
+    </section>
+
+
+    <?php
+
+if(isset($_POST['submit']))
+{
+  $count=0;
+  $res=mysqli_query($db,"SELECT * FROM `student` WHERE username='$_POST[username]' && password='$_POST[password]';");
+  $count=mysqli_num_rows($res);
+
+  if($count==0)
+  {
+    ?>
+          <!--
+          <script type="text/javascript">
+            alert("The username and password doesn't match.");
+          </script> 
+          -->
+      <div class="alert alert-danger" style="    width: 440px; margin-left: 852px; background-color: #de1313; color: white; margin-top: -210px;">
+        <strong>The username and password doesn't match</strong>
+      </div>    
+    <?php
+  }
+  else
+  {
+    $_SESSION['login_user'] = $_POST['username']; 
+
+    ?>
+      <script type="text/javascript">
+            window.location="index.php"
+      </script>
+   
+    <?php
+  }
+}
+
+?>
+</body>
+</html>
